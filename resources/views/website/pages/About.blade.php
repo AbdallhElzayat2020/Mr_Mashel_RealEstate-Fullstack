df841f72b411b05a866ef6e773ee9a60a37b5c9d
ahmdsadik <ahmed.sadik.dev@gmail.com>
2025-02-04 19:45:11 +0200

    About page

---
+++ resources/views/website/pages/About.blade.php
@@ -0,0 +1,305 @@
+@extends('website.layouts.master')
+@section('web_title', 'من نحن || قدرة العقارية')
+
+@section('content')
+    <section class="page-title p_relative pt_250 pb_170 centred"
+             style="background-image: url(assets/images/banner/Home_2.png);background-position: center;">
+        <div class="bg-layer p_absolute r_100 t_0">
+        </div>
+        <div class="large-container">
+            <div class="content-box p_relative d_block z_5">
+                <div class="title mb_25">
+                    <h1 class="d_block fs_68 lh_76 color_white fw_exbold">عن قدرة العقارية</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="about-style-three p_relative sec-pad" style="padding-top: 200px;">
+        <div class="large-container">
+            <div class="row clearfix">
+                <div class="col-lg-6 col-md-12 col-sm-12 content-column">
+                    <div class="content_block_5">
+                        <div class="content-box p_relative d_block mr_90">
+                            <div class="sec-title">
+                                <h2 class="p_relative d_block fs_25 lh_60 fw_bold"
+                                    style="color: #526652; text-align: start">
+                                    عن قدرة العقارية
+                                </h2>
+                            </div>
+
+                            <div class="text p_relative d_block mb_30">
+                                <ul style="list-style: bullet;">
+                                    <li style="color: #001d00; text-align: start; line-height: 2.2rem;">
+                                        <b style="font-weight: 800;">-</b>
+                                        تأسست <span style=" color: #C19C6E;">قدرة العقارية</span> في عام 2021م ,
+                                        وبفضل خبرة فريق إدارتهـا
+                                        الذي يمتلك خبرة
+                                        في <span style="color: #C19C6E;">السوق العقاري</span>
+                                        لأكثر من عشرين عاما استطاعت
+                                        قدرة العقارية
+                                        ان تثبت نفسها كاسم موثوق
+                                        في فترة قصيرة يستند الي
+                                        اعلى معايير الجودة والإحترافية
+                                    </li>
+                                    <br>
+                                    <li style="color: #001d00; text-align: start; line-height: 2.2rem;">
+                                        <b style="font-weight: 800;">-</b>
+                                        ﺗﺴﻌﻰ "قدرة العقارية " جاهدة لتلبية احتياجات ﻋﻤﻼﺋﻬﺎ وتحقيق أﻫﺪاﻓـﻬـﻢ
+                                        اﻻﺳـﺘـﺜﻤـﺎرﻳـﺔ في مجال العقارات
+                                    </li>
+                                    <br>
+                                    <li style="color: #001d00; text-align: start; line-height: 2.2rem;">
+                                        <b style="font-weight: 800;">-</b>
+                                        تعتبر "قدرة العقارية" من اواهل الشركات المرخصة والمنصفة والمصنفة من قبل
+                                        الهيئة العقارية في المملكة العربية السعودية
+                                    </li>
+                                    <br>
+                                    <li style="color: #001d00; text-align: start; line-height: 2.2rem;">
+                                        <b style="font-weight: 800;">-</b>
+                                        كما تقدم مجموعة واسعة من الخدمات في المجال العقاري بما في ذالك
+                                        <span style="color: #C19C6E;">
+                                                التسويق العقاري , الاستشارات العقارية , اتحاد الملاك , المزادات العقارية
+                                                , ادارة
+                                                وتشغيل العقارات
+                                            </span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12 image-column">
+                    <div class="image-box p_relative d_block pr_120 pb_150">
+                        <figure class="image image-1 p_relative d_block z_1 paroller"><img
+                                src="{{asset('assets/website/images/About.png')}}" alt="qudrah IMG">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="about  p_relative">
+        <div class="container">
+            <div class="row justify-content-between" style="gap: 15px">
+                <div class="col-lg-12">
+                    <div class="about_section">
+                        <div class="about_content">
+                            <h4>رؤيتنا</h4>
+                            <p>
+                                أن تُصبح <span>"قدرة العقارية"</span> الرائدة في المجال العقاري من خلال تحقيق الرؤية
+                                التامة
+                                لعملائها عبر تقديم مجموعة متكاملة من الخدمات المتميزة التي تحافظ على استدامة الأصول
+                                العقارية
+                                وتحقيق العوائد المرجوة.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-12">
+                    <div class="about_section">
+                        <div class="about_content">
+                            <h4>رسالتنا</h4>
+                            <p>
+                                في <span>"قدرة العقارية"</span> نُقدم لك خدمات عقارية متكاملة وفعّالة مُصممة لتلبية
+                                تطلعات عملائها، مع التركيز على تقديم تجربة إستثنائية تُضيف قيمة حقيقة عملائها.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="feature-style-two p_relative sec-pad">
+        <div class="intro text-center mb-5">
+            <div class="heading text-center font-weight-bold mb-3" style="color: #001d00; font-size: 40px">
+                لماذا تختار قدرة
+            </div>
+        </div>
+        <div class="large-container">
+            <div class="row clearfix">
+                <div class="col-lg-4 col-md-6 col-sm-12 feature-block mt-3">
+                    <div class="feature-block-two wow fadeInUp animated" data-wow-delay="00ms"
+                         data-wow-duration="1500ms">
+                        <div
+                            class="inner-box p_relative text-center d_block bg_white b_shadow_6 pt_60 pr_45 pb_55 pl_45 tran_5">
+                            <div class="icon-box p_relative d_block fs_100 lh_100 green_color mb_25 tran_5">
+                                <img src="{{asset('assets/website/images/مرخصة_ومعتمدة.png')}}" alt="مرخصة ومعتمدة" />
+                            </div>
+                            <h3 class="d_block fs_20 lh_30 fw_exbold mb_25 pb_25">
+                                <a href="index-3.html" class="d_iblock color_black hov_color"
+                                   style="color: #526652 !important">‫مرخصة‫ومعتمدة</a>
+                            </h3>
+                            <p class="text-center">
+                                ‫تعتبر‬‫شركة‬ ‫قدرة‬ ‫العقارية‬ ‫من‬ ‫أوائل‬ ‫الشركات‬
+                                ‫المرخصة‬ ‫والمصنفة‬ ‫من‬ ‫قبل‬ ‫الهيئة‬ ‫العامة‬ ‫للعقار‬
+                                ‫في‬ ‫المملكة‬ ‫العربية‬ ‫السعودية‬
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-12 feature-block mt-3">
+                    <div class="feature-block-two wow fadeInUp animated" data-wow-delay="00ms"
+                         data-wow-duration="1500ms">
+                        <div
+                            class="inner-box p_relative text-center d_block bg_white b_shadow_6 pt_60 pr_45 pb_55 pl_45 tran_5">
+                            <div class="icon-box p_relative d_block fs_100 lh_100 green_color mb_25 tran_5">
+                                <img src="{{asset('assets/website/images/الابتكار_والتطوير.png')}}" alt="الابتكار_والتطوير" />
+                            </div>
+                            <h3 class="d_block fs_20 lh_30 fw_exbold mb_25 pb_25">
+                                <a href="index-3.html" class="d_iblock color_black hov_color"
+                                   style="color: #526652 !important">الابتكار والتطوير</a>
+                            </h3>
+                            <p class="text-center">
+                                ‫تدمج‬‫قدرة‬ ‫العقارية‬ ‫التقنيات‬ ‫الحديثة‬ ‫في‬ ‫جميع‬
+                                ‫مراحل‬ ‫الخدمات‬ ‫العقارية‬ ‫لتحسين‬ ‫عمليات‬
+                                ‫ه‬‫ا‬‫وتقديم‬ ‫حلول‬ ‫جديدة‬ ‫وفعالة‬
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-12 feature-block mt-3">
+                    <div class="feature-block-two wow fadeInUp animated" data-wow-delay="00ms"
+                         data-wow-duration="1500ms">
+                        <div
+                            class="inner-box p_relative text-center d_block bg_white b_shadow_6 pt_60 pr_45 pb_55 pl_45 tran_5">
+                            <div class="icon-box p_relative d_block fs_100 lh_100 green_color mb_25 tran_5">
+                                <img src="{{asset('assets/website/images/الكفاءة_والاحترافية.png')}}" alt="الكفاءة_والاحترافية" />
+                            </div>
+                            <h3 class="d_block fs_20 lh_30 fw_exbold mb_25 pb_25">
+                                <a href="index-3.html" class="d_iblock color_black hov_color"
+                                   style="color: #526652 !important">‫الكفاءة ‬‫واالحترافية‬</a>
+                            </h3>
+                            <p class="text-center">
+                                ‫بسلسلة‬‫من‬ ‫الممارسات‬ ‫االحترافية‬ ‫المستدامة‪،‬‬ ‫تمكن‬
+                                ‫قدرة‬ ‫العقارية‬ ‫من‬ ‫رفع‬ ‫الكفاءة‬ ‫التشغيلية‬ ‫للعقار‬
+                                ‫وإطالة‬ ‫العمر‬ ‫الفعلي‬
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-6 col-sm-12 feature-block mt-3">
+                    <div class="feature-block-two wow fadeInUp animated" data-wow-delay="00ms"
+                         data-wow-duration="1500ms">
+                        <div
+                            class="inner-box p_relative text-center d_block bg_white b_shadow_6 pt_60 pr_45 pb_55 pl_45 tran_5">
+                            <div class="icon-box p_relative d_block fs_100 lh_100 green_color mb_25 tran_5">
+                                <img src="{{asset('assets/website/images/النزاهة.png')}}" alt="النزاهة" />
+                            </div>
+                            <h3 class="d_block fs_20 lh_30 fw_exbold mb_25 pb_25">
+                                <a href="index-3.html" class="d_iblock color_black hov_color"
+                                   style="color: #526652 !important">‫النزاهة</a>
+                            </h3>
+                            <p class="text-center">
+                                ‫نتمسك‬‫بأعلى‬ ‫المعايير‬ ‫األخالقية‬ ‫والشفافية‬ ‫في‬
+                                ‫جميع‬ ‫تعامالتنا‬ ‫مع‬ ‫عمالئنا‬
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 feature-block mt-3">
+                    <div class="feature-block-two wow fadeInUp animated" data-wow-delay="00ms"
+                         data-wow-duration="1500ms">
+                        <div
+                            class="inner-box p_relative text-center d_block bg_white b_shadow_6 pt_60 pr_45 pb_55 pl_45 tran_5">
+                            <div class="icon-box p_relative d_block fs_100 lh_100 green_color mb_25 tran_5">
+                                <img src="{{asset('assets/website/images/المسؤلية_والثقة.png')}}" alt="المسؤلية_والثقة" />
+                            </div>
+                            <h3 class="d_block fs_20 lh_30 fw_exbold mb_25 pb_25">
+                                <a href="index-3.html" class="d_iblock color_black hov_color"
+                                   style="color: #526652 !important">‫المسؤولية‬‫والثقة</a>
+                            </h3>
+                            <p class="text-center">
+                                ‫نسعى ‬‫لبناء‬ ‫علاقات‬ ‫متينة‬ ‫مع‬ ‫عمالئنا‬ ‫وشركاء‬
+                                ‫األعمال‬ ‫عبر‬ ‫تقديم‬ ‫األولية‬ ‫القصوى‬ ‫ل‬
+                                ‫مصالح‬‫هم‬‫المتنوعة‬
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="vision-mission-section p_relative">
+        <div class="container">
+            <div class="heading text-center font-weight-bold mb-3" style="color: #001d00; font-size: 40px">
+                اهدافنا
+            </div>
+            <div class="row justify-content-between align-items-center">
+                <div class="col-lg-6 col-md-12">
+                    <div class="vision-card">
+                        <div class="vision-content">
+                            <h4 style="color: #C19C6E;">رضا العملاء</h4>
+                            <p>
+                                تقديم خدمة إستثنائية لعملائنا وتلبية إحتياجاتهم وتحقيق تطلعاتهم
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-12">
+                    <div class="vision-card">
+                        <div class="vision-content">
+                            <h4 style="color: #C19C6E;">الجودة والتميز</h4>
+                            <p>
+                                تحقيق أعلى مستويات الجودة والتميز في الخدمات العقارية
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <div class="vision-card">
+                        <div class="vision-content">
+                            <h4 style="color: #C19C6E;">الشراكات الإستراتيجية</h4>
+                            <p>
+                                تطوير شراكات إستراتيجية مع الشركات والجهات الحكومية لتعزيز قدراتنا وتوسيع نطاقنا
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <div class="vision-card">
+                        <div class="vision-content">
+                            <h4 style="color: #C19C6E;">إستدامة العقار</h4>
+                            <p>
+                                بتقديم إهتماماً كبيراً لصيانة وإدارة العقارات بطريقة تضمن إستدامتها لفترة طويلة </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="subscribe-section p_relative pl_60 pr_60">
+        <div class="bg-shape p_absolute l_0 b_0"></div>
+        <div class="outer-container p_relative bg_yellow pt_60 pb_60">
+            <div class="pattern-layer p_absolute l_0 b_0 r_0"></div>
+            <div class="large-container">
+                <div class="row clearfix">
+                    <div class="col-lg-6 col-md-12 col-sm-12 text-column">
+                        <div class="text">
+                            <h2 class="d_block fs_30 lh_50 color_white fw_exbold">اشترك ليصلك كل جديد</h2>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 col-sm-12 form-column">
+                        <div class="form-inner p_relative d_block ml_40 mt_8">
+                            <form action="https://azim.commonsupport.com/Amortez/contact.html" method="post">
+                                <div class="form-group p_relative d_block bg_white mr-0 p_13 b_radius_3 pr_100">
+                                    <input type="email" name="Email" placeholder="البريد الالكتروني" required>
+                                    <button type="submit"><i class="fas fa-envelope-open"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
