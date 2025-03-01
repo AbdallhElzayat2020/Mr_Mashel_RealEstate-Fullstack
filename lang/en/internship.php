5a5c283cb1cbc1e9133308fae1cec984a1483d2f
ahmdsadik <ahmed.sadik.dev@gmail.com>
2025-03-01 14:42:36 +0200

    intern

---
+++ lang/en/internship.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    'page_title' => 'Qudra Real Estate || Training',
+    'bread_crumb_1' => 'Home',
+    'bread_crumb_2' => 'Training',
+    'header' => 'Training',
+
+    'intern_1' => 'Step into the world of real estate with
+                   Qudra Training Program!',
+
+    'intern_1_body' => 'You surely aspire to build a successful career in the real estate field.',
+    'intern_2_body' => 'At Qudra Real Estate, we offer you a unique opportunity to start your journey professionally.',
+    'intern_3_body' => 'Join our team with exceptional job opportunities!',
+
+    'why_choose_our_program' => 'Why Choose Our Program?',
+    'why_choose_our_program_body_1' => '• Hands-on and professional training led by industry experts.',
+    'why_choose_our_program_body_2' => '• Comprehensive qualification covering everything you need to excel in the market.',
+    'why_choose_our_program_body_3' => '• A real opportunity to turn your passion into a future career.',
+
+    'form_header' => 'The opportunity is here 🚀!',
+    'form_info' => 'Fill in your details and let’s start the journey to success together.',
+
+    'form_input_1' => 'Full Name',
+    'form_input_2' => 'Phone Number',
+    'form_input_3' => 'Email',
+    'form_input_4' => 'Educational Qualification',
+    'form_input_5' => 'Years of Experience',
+    'form_input_6' => 'Field of Experience (if any)',
+    'form_input_7' => 'Upload Resume',
+    'form_input_8' => 'Notes',
+    'form_input_9' => '
+                       I acknowledge that I have read the <a href="#">terms and conditions</a>
+                       of Qudra Real Estate.',
+    'form_input_10' => 'Submit',
+
+    'option_1' => 'No prior experience',
+    'option_2' => '1 to 3 years',
+    'option_3' => '3 to 5 years',
+    'option_4' => '5 to 10 years',
+];
